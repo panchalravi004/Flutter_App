739ad260265a9f0df39b5786b9819bba135fd1ef
Ravi Panchal <panchalravi018@gmail.com>
2022-01-23 14:27:46 +0530

    catelog add first product

---
+++ lib/models/catelog.dart
@@ -0,0 +1,10 @@
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item({required this.id, required this.name,required this.desc,required this.price,required this.color,required this.image});
+}
